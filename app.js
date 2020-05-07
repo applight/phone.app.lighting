2d9db2ca6ec025fd0813a800faabb497ca2b9106
admin <admin@app.lighting>
2020-05-07 18:16:33 -0400

    remane index.js app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+const express  = require('express');
+const twilio   = require('twilio');
+const client   = require('twilio')(process.env.TWILIO_ACCOUNT_SID,
+				   process.env.TWILIO_AUTH_TOKEN);
+const VoiceResponse     = twilio.twiml.VoiceResponse;
+const MessagingResponse = twilio.twiml.MessagingResponse;
+const ClientCapability  = twilio.jwt.ClientCapability;
+const app               = express();
+
+	
+app.get('/voice-token', (req, res) => {
+    const identity = 'the_user_id';
+    
+    const capability = new ClientCapability({
+	'accountSid': process.env.TWILIO_ACCOUNT_SID,
+	'authToken': process.env.TWILIO_AUTH_TOKEN
+    });
+
+    capability.addScope(new ClientCapability.IncomingClientScope(identity));   
+    capability.addScope(new ClientCapability.OutgoingClientScope({
+	'applicationSid': process.env.TWILIO_TWIML_APP_SID,
+	'clientName': identity
+    }) );
+    
+    // Set headers in response
+    res.setStatusCode(200);
+    res.appendHeader('Access-Control-Allow-Origin', 'https://phone.app.lighting');
+    res.appendHeader('Access-Control-Allow-Methods', 'GET');
+    res.appendHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.appendHeader("Content-Type", "application/json");
+
+    // Include token in a JSON response
+    res.send({
+	'identity': identity,
+	'token': capability.toJwt()
+    });
+});
